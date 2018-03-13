5befb606ea1f90e4d2096824f7e45e39eed54a72
Dmitri Lakachauskis <lakiboy83@gmail.com>
2018-03-13 13:28:21 +0200

    Update examples.

--- examples/download_passport_check.php
+++ examples/download_passport_check.php
@@ -1,20 +1,16 @@
 <?php
+// Usage:
+// $ php examples/download_passport_check.php > result.pdf
 
 declare(strict_types=1);
 
 use Damax\Client\Configuration;
-use Http\Adapter\Guzzle6\Client;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $config = new Configuration('https://product.damax.solutions/api', 'token');
 
-$httpClient = Client::createWithConfig([
-    'verify' => false,
-]);
-
 echo (string) $config
-    ->setHttpClient($httpClient)
     ->getClient()
     ->downloadPassportCheck('74 05 558551')
 ;
--- examples/passport_check.php
+++ examples/passport_check.php
@@ -1,20 +1,16 @@
 <?php
+// Usage:
+// $ php examples/passport_check.php
 
 declare(strict_types=1);
 
 use Damax\Client\Configuration;
-use Http\Adapter\Guzzle6\Client;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $config = new Configuration('https://product.damax.solutions/api', 'token');
 
-$httpClient = Client::createWithConfig([
-    'verify' => false,
-]);
-
 $result = $config
-    ->setHttpClient($httpClient)
     ->getClient()
     ->checkPassport('74 05 558551')
 ;
